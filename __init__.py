e6c8a1d1fb620415dcbc8a8f3bb31d59159fa88e
tsmanner <zephrincochrane@gmail.com>
2017-09-28 20:49:37 -0400

    Started implementing DataFrame based 5e class representation.

--- __init__.py
+++ __init__.py
@@ -1,72 +1,82 @@
-from app import *
+import pandas as pd
+
+
+def rogue():
+    levels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    proficiency = [2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6]
+    sneak_dice = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10]
+    features = [
+        ["Expertise", "Sneak Attack", "Thieves' Cant"],
+        ["Cunning Action"],
+        [],
+        ["Ability Score"],
+        ["Uncanny Dodge"],
+        ["Expertise"],
+        ["Evasion"],
+        ["Ability Score"],
+        [],
+        ["Ability Score"],
+        ["Reliable Talent"],
+        ["Ability Score"],
+        [],
+        ["Blindsense"],
+        ["Slippery Mind"],
+        ["Ability Score"],
+        [],
+        ["Elusive"],
+        ["Ability Score"],
+        ["Stroke of Luck"],
+    ]
+    return pd.DataFrame(data={"proficiency": proficiency,
+                              "sneak_dice": sneak_dice,
+                              "features": features},
+                        index=levels)
+
+
+def arcane_trickster():
+    levels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    cantrips = [None, None, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+    spells_known = [None, None, 3, 4, 4, 4, 5, 6, 6, 7, 8, 8, 9, 10, 10, 11, 11, 11, 12, 13]
+    slots_1 = [None, None, 2, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+    slots_2 = [None, None, None, None, None, None, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    slots_3 = [None, None, None, None, None, None, None, None, None, None, None, None, 2, 2, 2, 3, 3, 3, 3, 3]
+    slots_4 = [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, 1, 1]
+    features = [
+        [],
+        [],
+        ["Mage Hand Legerdemain"],
+        [],
+        [],
+        [],
+        [],
+        [],
+        ["Magical Ambusher"],
+        [],
+        [],
+        [],
+        ["Versatile Trickster"],
+        [],
+        [],
+        [],
+        ["Spell Thief"],
+        [],
+        [],
+        [],
+    ]
+    return pd.DataFrame(data={"cantrips": cantrips,
+                              "spells_known": spells_known,
+                              "slots_1": slots_1,
+                              "slots_2": slots_2,
+                              "slots_3": slots_3,
+                              "slots_4": slots_4,
+                              "features": features},
+                        index=levels)
+
 
 if __name__ == '__main__':
-    stats = {
-        "strength": 13,
-        "dexterity": 16,
-        "constitution": 10,
-        "intelligence": 12,
-        "wisdom": 17,
-        "charisma": 6,
-    }
-
-    char = Character(Halfling)  # Base
-    char.equipment.append(gloves_of_dexterity(2))
-    char.equipment.append(periapt_of_wisdom(2))
-    char.equipment.append(cloak_of_resistance(1))
-    char.equipment.append(ring_of_ua_reduce_person_1_str)
-
-    """ Level 1 """
-    char.level_up(Ninja, stats)
-    char.feats[1] = [point_blank_shot]
-    char.flaws = [noncombatant(precise_shot), vulnerable(weapon_focus("dart"))]
-    char.feats[1].extend([precise_shot, weapon_focus("dart")])
-    """ Level 2 """
-    char.level_up(Wizard)
-    """ Level 3 """
-    char.level_up(Ninja)
-    char.feats[3] = [craven]
-    """ Level 4 """
-    stats["wisdom"] += 1
-    char.level_up(Wizard, stats)
-    """ Level 5 """
-    char.level_up(Ninja)
-    """ Level 6 """
-    char.level_up(Wizard)
-    char.feats[6] = [fiery_burst]
-    """ Level 7 """
-    char.level_up(Ninja)
-    """ Level 8 """
-    stats["intelligence"] += 1
-    char.level_up(Wizard, stats)
-    """ Level 9 """
-    char.level_up(Wizard)  # partial(Wizard, feat=invisible_needle))
-    char.feats[9] = [invisible_needle]
-    """ Level 10 """
-    # char.level_up(MasterThrower)  # Level 10
-    """ Level 11 """
-    # char.level_up(Ninja)  # Level 11
-    """ Level 12 """
-    # stats["intelligence"] += 1
-    # char.level_up(Ninja, stats)  # Level 12
-    # char.feats[12]
-    """ Level 13 """
-    # char.level_up(Wizard)  # Level 13
-    """ Level 14 """
-    # char.level_up(Wizard)  # Level 14
-    """ Level 15 """
-    # char.level_up(Ninja)  # Level 15
-    """ Level 16 """
-    # stats["intelligence"] += 1
-    # char.level_up(Wizard, stats)  # Level 16
-    """ Level 17 """
-    # char.level_up(Ninja)  # Level 17
-    """ Level 18 """
-    # char.level_up(Wizard)  # Level 18
-    """ Level 19 """
-    # char.level_up(Ninja)  # Level 19
-    """ Level 20 """
-    # stats["intelligence"] += 1
-    # char.level_up(Wizard, stats)  # Level 20
-
-    print(char.__str__(verbose=True))
+    r = rogue()
+    at = arcane_trickster()
+    char = r.join(at, rsuffix="_r", how="outer")
+    char["features"] = char["features"] + char["features_r"]
+    char = char.drop(["features_r"], axis=1)
+    print(char)
